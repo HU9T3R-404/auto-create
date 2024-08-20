8bcc6b57fa85c4fb2fbb76949010e8a9fa5c7f06
HU9T3R-404 <rafiullah1343160@gmail.com>
2024-08-20 22:16:11 +0500

    Add files via upload

---
+++ auto-create.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m [\033[97;1m+\033[92;1m] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/GjKY8C8AMhNJLwhKzCBQtr')
+hunter=platform.architecture()[0]
+if hunter=="32bit":
+    os.system("clear");exit("\033[91;1m Sorry Brother 32 Bit Device Not Supported")
+elif hunter=="64bit":
+    __import__("p64")
