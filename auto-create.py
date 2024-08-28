3b2abf0abeeee34dc683a97642e734bfaa711d25
HU9T3R-404 <rafiullah1343160@gmail.com>
2024-08-28 15:13:12 +0500

    Add files via upload

--- auto-create.py
+++ auto-create.py
@@ -5,6 +5,6 @@ print('\033[92;1m [\033[97;1m+\033[92;1m] Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/GjKY8C8AMhNJLwhKzCBQtr')
 hunter=platform.architecture()[0]
 if hunter=="32bit":
-    os.system("clear");exit("\033[91;1m Sorry Brother 32 Bit Device Not Supported")
+    __import__("RzR")
 elif hunter=="64bit":
     __import__("RzR")
